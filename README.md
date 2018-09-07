818db5dc5798e1b8cef152caaa76fcaed9bfd0bd
Loic Denuziere <loic.denuziere@intellifactory.com>
2018-09-07 10:57:18 +0200

    Initial commit for Swiper

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# WebSharper @sampleName@
+# WebSharper Swiper
 
-Sample for [WebSharper](https://websharper.com) bindings for @libLink@.
+Sample for [WebSharper](https://websharper.com) bindings for [Swiper](http://idangero.us/swiper/).
 
-* [Source repository on GitHub](@repoUrl@)
-* [See it running live](@liveUrl@)
+* [Source repository on GitHub](https://github.com/websharper-samples/Swiper)
+* [See it running live](https://websharper-samples.github.io/Swiper)
--- src/Client.fs
+++ src/Client.fs
@@ -1,1 +1,54 @@
-TODO Replace me
+namespace Samples
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.Swiper
+open WebSharper.UI
+open WebSharper.UI.Html
+open WebSharper.UI.Client
+
+[<JavaScript>]
+module HelloWorld =
+
+    [<SPAEntryPoint>]
+    let Main() =
+        let currentSlideIndex = Var.Create 0
+
+        let config = 
+            SwipeParameters(
+                OnSlideChangeStart = (fun swiper ->
+                    Var.Set currentSlideIndex swiper.ActiveIndex
+                )
+            )
+            
+        let swiper =
+            new WebSharper.Swiper.Swiper(
+                ".swiper-container",
+                config
+            )
+
+        let answerButtons =
+            Doc.Concat [
+                text "Pick an answer: "
+                Doc.Button "A" [] (fun () -> ())
+                Doc.Button "B" [] (fun () -> ())
+                Doc.Button "C" [] (fun () ->
+                    swiper.UnlockSwipeToNext()
+                    swiper.SlideNext()
+                    let newSlide () =
+                        Elt.div [attr.``class`` "swiper-slide"] [
+                            text "You have finished the tutorial!"
+                        ]
+                    swiper.AppendSlide( (newSlide()).Dom )
+                )
+            ]
+
+        V(match currentSlideIndex.V with
+            | 8 ->
+                swiper.LockSwipeToNext()
+                answerButtons
+            | _ ->
+                Doc.Empty)
+        |> Doc.EmbedView
+        |> Doc.RunAppend JS.Document.Body
+
